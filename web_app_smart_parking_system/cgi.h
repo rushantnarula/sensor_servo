1b40d0f2ab9d1defa1d3fa1336b5495ccd9bcf01
Your Name <you@example.com>
2025-01-15 15:16:21 +0100

    Add updated cgi.h with revised endpoint definitions.

---
+++ web_app_smart_parking_system/cgi.h
@@ -0,0 +1,32 @@
+#ifndef TT_CGI_H
+#define TT_CGI_H 1
+
+/* Ensure required dependencies are included */
+#if !LWIP_HTTPD_CUSTOM_FILES
+#error This needs LWIP_HTTPD_CUSTOM_FILES
+#endif
+
+#if !LWIP_HTTPD_DYNAMIC_HEADERS
+#error This needs LWIP_HTTPD_DYNAMIC_HEADERS
+#endif
+
+#if !LWIP_HTTPD_CGI
+#error This needs LWIP_HTTPD_CGI
+#endif
+
+/* Endpoints for the parking system */
+// #define ENTER_UI_ENDPOINT "/enter.html"
+#define PARKING_UI_ENDPOINT "/parking.html"
+#define ERROR_404_ENDPOINT "/404.html"
+
+#define PARKING_STATUS_ENDPOINT "/check_availability"
+#define RESERVE_SPOT_ENDPOINT "/reserve_spot"
+#define ENTER_PARKING_ENDPOINT "/enter_parking"
+
+#define TEST_ENDPOINT "/test" /* Optional: This just for testing, no need for this */
+
+/* Initialization functions */
+void custom_files_init(void);
+void cgi_init(void);
+
+#endif /* TT_CGI_H */
